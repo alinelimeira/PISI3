0c839e47a1e177d597fc6539bdca6e5e3700079d
Fellipe <fellipe1311.fb@gmail.com>
2023-02-26 17:56:21 -0300

    Bug fixes and half textField widget

--- flutter_four_learn_app/lib/src/MainMenuScreen.dart
+++ flutter_four_learn_app/lib/src/MainMenuScreen.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_four_learn_app/util/textFields.dart';
 
 class MainMenuScreen extends StatefulWidget {
   const MainMenuScreen({Key? key}) : super(key: key);
@@ -12,8 +15,33 @@ class MainMenuScreen extends StatefulWidget {
 class _MainMenuScreenState extends State<MainMenuScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(child: Text('Main menu screen')),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/4learnLogo.png',
+              fit: BoxFit.contain,
+              height: 32,
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blue,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+          child: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(children: const [
+          StandardTextField(
+            title:
+                'Olá, eu sou o 4Learn e tenho como objetivo lhe auxiliar nos estudos durante sua jornada no Ensino Médio!',
+          )
+        ]),
+      )),
     );
   }
 }
---
+++ flutter_four_learn_app/lib/util/textFields.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class StandardTextField extends StatefulWidget {
+  final String title;
+  final Color color;
+  final Color textColor;
+  final double fontSize;
+  const StandardTextField(
+      {required this.title,
+      this.color = const Color(0xFF44CDEB),
+      this.textColor = Colors.black,
+      this.fontSize = 16,
+      super.key});
+
+  @override
+  State<StandardTextField> createState() => _StandardTextFieldState();
+}
+
+class _StandardTextFieldState extends State<StandardTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 292,
+      height: 121,
+      decoration: BoxDecoration(
+        color: widget.color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        widget.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: widget.fontSize),
+      ),
+    );
+  }
+}
