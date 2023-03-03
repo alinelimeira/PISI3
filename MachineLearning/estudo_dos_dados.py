4693facd33a1a740807089dd5527dfc6a97b38ee
Aline <alinesantanalimeira@gmail.com>
2023-03-03 10:43:10 -0300

    alteracao das variaveis

--- MachineLearning/estudo_dos_dados.py
+++ MachineLearning/estudo_dos_dados.py
@@ -43,14 +43,14 @@ topo1 = dados_final.head()
 #renomeando as colunas antigas e as novas 
 
 a_renomear = {
-    "parental level of education_associate's degree": 'level_associado',
-    "parental level of education_bachelor's degree": 'level_bacharel',
-    "parental level of education_high school": 'level_high_school',
-    "parental level of education_master's degree": 'level_master',
-    "parental level of education_some college" : 'level_some_college ',
-    "parental level of education_some high school" : 'level_some_high_school',
-    "test preparation course_completed" : 'teste_preparation_course_completed',
-    "test preparation course_none" : "test_preparation_course_none",
+    "parental level of education_associate's degree": 'nivel_tecnico',
+    "parental level of education_bachelor's degree": 'nivel_bacharel',
+    "parental level of education_high school": 'ensino_medio',
+    "parental level of education_master's degree": 'nivel_mestre',
+    "parental level of education_some college" : 'nivel_tecnologo ',
+    "parental level of education_some high school" : 'ensino_medio_incompleto',
+    "test preparation course_completed" : 'teste_preparacao_completo',
+    "test preparation course_none" : "sem_teste_preparacao",
     "math percentage" : "math_percentage",
     "reading score percentage": "reading_score_percentage",
     "writing score percentage" : "writing_score_percentage",
@@ -59,7 +59,8 @@ a_renomear = {
     "race/ethnicity_group C" : "race_ethnicity_group_C",
     "race/ethnicity_group D": "race_ethnicity_group_D",
     "race/ethnicity_group E": "race_ethnicity_group_E",
-    "lunch_free/reduced" : "lunch_free_reduced"
+    "lunch_free/reduced" : "almoco_gratis",
+    "lunch_standard": "almoco_padrao"
 
 
 }
@@ -69,24 +70,24 @@ dados_final = dados_final.rename(columns = a_renomear)
 
 #writing_score_percentage e reading_score_percentage estão em valores separados, entao eh necessario fazer uma media das duas notas
 #atravez do mean ()
-dados_final['average_score_languages'] = dados_final[['reading_score_percentage','writing_score_percentage']].mean(axis=1)
+dados_final['nota_escrita_leitura'] = dados_final[['reading_score_percentage','writing_score_percentage']].mean(axis=1)
 
 #apos calular eh necessario tirar as casas decimas e deixar duas casas apos a virgula
-dados_final['average_score_languages'] = dados_final['average_score_languages'].round(2)
+dados_final['nota_escrita_leitura'] = dados_final['nota_escrita_leitura'].round(2)
 
 
 #colocando a math_percentage e average_score_languages em categótica
 
 
-dados_final['math_percentage_class'] = (dados_final['math_percentage'] > 0.7).astype(int)
-dados_final['average_score_languages_class'] = (dados_final['average_score_languages'] > 0.7).astype(int)
+dados_final['nota_matematica'] = (dados_final['math_percentage'] > 0.7).astype(int)
+dados_final['nota_linguagem'] = (dados_final['nota_escrita_leitura'] > 0.7).astype(int)
 
 #APAGANDO AS COLUNAS QUE RESTARAM COM DADOS DECIMAIS
-dados_final = dados_final.drop(['math_percentage','average_score_languages' ,'reading_score_percentage','writing_score_percentage'], axis=1)
+dados_final = dados_final.drop(['math_percentage','nota_escrita_leitura' ,'reading_score_percentage','writing_score_percentage'], axis=1)
 
 #SEPARANDO AS ETNIAS
-dados_final['white_people'] = dados_final['race_ethnicity_group_A'] + dados_final['race_ethnicity_group_B'] + dados_final['race_ethnicity_group_C']
-dados_final['non_white_people'] = dados_final['race_ethnicity_group_D'] + dados_final['race_ethnicity_group_E']
+dados_final['pessoas_brancas'] = dados_final['race_ethnicity_group_A'] + dados_final['race_ethnicity_group_B'] + dados_final['race_ethnicity_group_C']
+dados_final['pessoas_pardas_pretas'] = dados_final['race_ethnicity_group_D'] + dados_final['race_ethnicity_group_E']
 
 #APAGANDO COLUNAS ANTIGAS DAS ETNIAS
 dados_final = dados_final.drop(['race_ethnicity_group_A','race_ethnicity_group_B' ,'race_ethnicity_group_C','race_ethnicity_group_D', 'race_ethnicity_group_E'], axis=1)
