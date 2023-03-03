5029a7afc8eba29ddef7cc7cdfef13e0c93e09af
Aline <alinesantanalimeira@gmail.com>
2023-03-03 17:29:38 -0300

    add informacoes etnia

--- MachineLearning/app.py
+++ MachineLearning/app.py
@@ -17,26 +17,31 @@ st.subheader("Esta página tem como objetivo mostrar a analise dos dados do data
 classifier_name = st.sidebar.selectbox("Selecione um classificador", ("Árvore de decisão", "Bernoulli","KNN"))
 
 
-dados = pd.read_csv("MachineLearning/dataset/Student_Performance_new.csv")
+dados = pd.read_csv("../MachineLearning/dataset/Student_Performance_new.csv")
 
 
 st.dataframe(dados)  # comando q aparece toda a tabela
 
-st.write("Tipos das colunas: ")
+st.markdown(" <p style = 'font-size: 20px' >Tipos das colunas: </p> ", unsafe_allow_html=True)
 st.table(dados.dtypes)
 
-st.write("Verificando se há valores nulos: ")
+st.markdown("<p style = 'font-size: 20px' > Verificando se há valores nulos: </p> ", unsafe_allow_html=True)
 st.table(dados.isnull().sum())
 
+st.markdown("<p style = 'font-size: 20px' > Verificando se há valores repetidos: </p> ", unsafe_allow_html=True)
+duplicatas =  dados.duplicated()
+num_duplicatas = duplicatas.sum()
+st.table(pd.DataFrame([num_duplicatas]))
+
 ##tabela mulher x homem
-plt.title("Analisando a quantidade de mulheres e homens: ")
+st.write("<p  style = 'font-size: 20px'> Analisando a quantidade de mulheres e homens: </p> ", unsafe_allow_html=True)
 counts = dados['sex'].value_counts()
 colors = ['#D2103C', '#00ACEE']
 
 plt.xlabel('Sexo')
 plt.ylabel('Quantidade')
 
-plt.bar(counts.index, counts.values, color = colors, width = 0.6)
+plt.bar(counts.index, counts.values, color = colors, width = 0.7)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
 
@@ -53,10 +58,22 @@ plt.legend(counts.index, bbox_to_anchor = (1, 1))
 st.pyplot(fig)
 
 
+
+st.markdown("<p  style = 'font-size: 20px'> Cada grupo de etnia é separado da seguinte forma:</p> ", unsafe_allow_html=True)
+st.markdown("Group A: White - British") 
+st.markdown("Group B: White - Irish") 
+st.markdown("Group C: White - White - Any other white background") 
+st.markdown("Group D: Mixed - White and Black Caribbean") 
+st.markdown("Group E: Mixed - White and Black African ") 
+
+
+
 ## nivel de educacao dos pais
+
+st.markdown("<p  style = 'font-size: 20px'> Nível de educação dos pais:</p> ", unsafe_allow_html=True)
 plt.title("Nivel de education dos pais")
 counts = dados ['parental level of education']. value_counts()
-colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF']
+colors = ['#9e4a49', '#9e4a49', '#9e4a49', '#9e4a49', '#9e4a49', '#9e4a49']
 
 
 fig, ax = plt.subplots(figsize =(12,7),layout='constrained')
